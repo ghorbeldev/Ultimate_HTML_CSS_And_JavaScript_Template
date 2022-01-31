788429539aa23ce413f62803174a3cc22888f95d
ghorbeldev <adem.ghorbel9@gmail.com>
2022-01-31 19:07:29 +0100

    Make Site More responsive + add some javascript + custom the scrollbar style

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 let otherLinks = document.querySelector('.other-links');
+let skillsSection = document.querySelector('.skills');
 let skills = document.querySelectorAll('.skills .progress');
 
 document.onmouseover = e => {
@@ -19,15 +20,60 @@ const scrollDown = 'scroll-down';
 let currentScroll = window.pageYOffset;
 let lastScroll = 0;
 
+const second = 1000,
+	minute = second * 60,
+	hour = minute * 60,
+	day = hour * 24;
+let days = document.getElementById('days');
+let hours = document.getElementById('hours');
+let minutes = document.getElementById('minutes');
+let seconds = document.getElementById('seconds');
+let eventDate = new Date().setDate(new Date().getDate() + 15);
+let countDown = new Date(eventDate).getTime();
+const counterInterval = setInterval(() => {
+	let now = new Date().getTime();
+	let distance = countDown - now;
+
+	days.innerText = Math.floor(distance / day);
+	hours.innerText = Math.floor((distance % day) / hour);
+	minutes.innerText = Math.floor((distance % hour) / minute);
+	seconds.innerText = Math.floor((distance % minute) / second);
+
+	//do something later when date is reached
+	if (distance < 0) {
+		clearInterval(counterInterval);
+	}
+}, 0);
+
+// Increase Stats on scroll
+let statsSection = document.querySelector('.stats');
+let countStarted = false;
+let stats = document.querySelectorAll('.stats .box .number');
+function count(element) {
+	let goal = +element.dataset.goal;
+	let countInterval = setInterval(() => {
+		element.textContent++;
+		if (+element.textContent == goal) {
+			clearInterval(countInterval);
+		}
+	}, 10);
+}
+
 window.addEventListener('scroll', () => {
 	currentScroll = window.pageYOffset;
-	skills.forEach(skill => {
-		if (currentScroll + 250 > skill.offsetTop) {
+	if (currentScroll >= skillsSection.offsetTop - 280) {
+		skills.forEach(skill => {
 			skillStyle = getComputedStyle(skill);
 			progressWidth = skillStyle.getPropertyValue('--skill-progress');
 			skill.children[0].style.width = progressWidth + '%';
+		});
+	}
+	if (currentScroll >= statsSection.offsetTop - 280) {
+		if (!countStarted) {
+			stats.forEach(ele => count(ele));
+			countStarted = true;
 		}
-	});
+	}
 	if (currentScroll <= 0) {
 		document.body.classList.remove(scrollUp);
 		return;
@@ -50,33 +96,3 @@ window.addEventListener('scroll', () => {
 	}
 	lastScroll = currentScroll;
 });
-
-const second = 1000,
-	minute = second * 60,
-	hour = minute * 60,
-	day = hour * 24;
-
-// console.log('Second: ' + second);
-// console.log('Minute: ' + minute);
-// console.log('Hour: ' + hour);
-// console.log('Day: ' + day);
-let days = document.getElementById('days');
-let hours = document.getElementById('hours');
-let minutes = document.getElementById('minutes');
-let seconds = document.getElementById('seconds');
-let eventDate = new Date().setDate(new Date().getDate() + 15);
-let countDown = new Date(eventDate).getTime();
-const counterInterval = setInterval(() => {
-	let now = new Date().getTime();
-	let distance = countDown - now;
-
-	days.innerText = Math.floor(distance / day);
-	hours.innerText = Math.floor((distance % day) / hour);
-	minutes.innerText = Math.floor((distance % hour) / minute);
-	seconds.innerText = Math.floor((distance % minute) / second);
-
-	//do something later when date is reached
-	if (distance < 0) {
-		clearInterval(counterInterval);
-	}
-}, 0);
